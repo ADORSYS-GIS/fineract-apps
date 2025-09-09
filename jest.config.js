06e4ca855c0ec190793c7c77a7d8f5d4b46ed501
NkwaTambe <nkwajudetambe@gmail.com>
2025-09-09 16:08:53 +0100

    feat(testing): set up centralized Jest testing environment

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+export default {
+	preset: "ts-jest",
+	testEnvironment: "jsdom",
+	setupFilesAfterEnv: ["@testing-library/jest-dom"],
+	moduleNameMapper: {
+		"^@repo/ui/(.*)$": "<rootDir>/packages/ui/src/$1",
+	},
+	transform: {
+		"^.+\\.(ts|tsx)$": "ts-jest",
+	},
+	testMatch: ["<rootDir>/packages/**/?(*.)+(spec|test).[jt]s?(x)"],
+};
