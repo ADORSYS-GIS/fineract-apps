ffb70f4e0b3eba2499ecfef04869326a6e8218ea
NkwaTambe <nkwajudetambe@gmail.com>
2025-09-10 15:30:43 +0100

    feat(ci): integrate SonarQube for code quality analysis

--- jest.config.js
+++ jest.config.js
@@ -5,11 +5,24 @@ export default {
 	roots: ["<rootDir>/frontend", "<rootDir>/packages"],
 	moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
 	transform: {
-		"^.+\\.(ts|tsx)$": "ts-jest",
+		"^.+\\.(ts|tsx)$": [
+			"ts-jest",
+			{
+				tsconfig: "<rootDir>/packages/config/tsconfig.base.json",
+			},
+		],
 	},
 	moduleNameMapper: {
 		"^@fineract-apps/(.*)$": "<rootDir>/packages/$1/src",
 	},
 	setupFilesAfterEnv: ["@testing-library/jest-dom"],
 	testPathIgnorePatterns: ["/node_modules/", "/dist/"],
+	testMatch: [
+		"<rootDir>/frontend/**/?(*.)+(spec|test).[jt]s?(x)",
+		"<rootDir>/packages/**/?(*.)+(spec|test).[jt]s?(x)",
+	],
+	collectCoverage: true,
+	coverageReporters: ["lcov", "text"],
+	coverageDirectory: "coverage",
+	coveragePathIgnorePatterns: ["/node_modules/", "/dist/"],
 };
