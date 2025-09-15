258132bc339bac775dcb8c7ca337ef0114d5d9b6
NkwaTambe <nkwajudetambe@gmail.com>
2025-09-15 10:09:09 +0100

    fix(tailwind): configure content scanning for shared ui package

--- frontend/account-manager-app/src/App.tsx
+++ frontend/account-manager-app/src/App.tsx
@@ -1,8 +1,20 @@
+import { Button } from "@fineract-apps/ui";
+
 function App() {
 	return (
 		<div style={{ padding: "20px" }}>
 			<h1>Account Manager App</h1>
-			<p>This button is a shared component from the '@repo/ui' package.</p>
+			<p>
+				This button is a shared component from the '@fineract-apps/ui' package.
+			</p>
+			<Button>Click me!</Button>
+			<Button>Click me!</Button>
+
+			<Button>Click me!</Button>
+
+			<Button>Click me!</Button>
+
+			<Button className="bg-red-500">Click me!</Button>
 		</div>
 	);
 }
