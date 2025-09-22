103e6c841c74de7996cea19023b74e6c84123a5e
nancymuyeh <nancymuyeh@gmail.com>
2025-09-22 08:07:52 +0100

    fix(ui): remove unused dependencies

--- frontend/account-manager-app/src/App.tsx
+++ frontend/account-manager-app/src/App.tsx
@@ -1,30 +1,15 @@
 import "@fineract-apps/ui/styles.css";
-import { Button, SearchBar } from "@fineract-apps/ui";
-import { useState } from "react";
+import { Button } from "@fineract-apps/ui";
 
 function App() {
-	const [searchValue, setSearchValue] = useState("");
-
 	return (
 		<div style={{ padding: "20px" }}>
 			<h1>Account Manager App</h1>
 			<p>
 				This button is a shared component from the '@fineract-apps/ui' package.
 			</p>
-			<SearchBar
-				value={searchValue}
-				onValueChange={setSearchValue}
-				placeholder="Search transactions..."
-				variant="withButton"
-			/>
-			<Button>Click me!</Button>
-			<Button>Click me!</Button>
 
 			<Button>Click me!</Button>
-
-			<Button>Click me!</Button>
-
-			<Button className="bg-red-500">Click me!</Button>
 		</div>
 	);
 }
