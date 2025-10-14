c6f72ca078c2ac49ac97dbf763e593bcc95f0ec4
Elwizzy12 <chiahans123@gmail.com>
2025-10-14 13:51:14 +0100

    chore: added script to suppress warnings from generated code

--- frontend/account-manager-app/src/services/api.ts
+++ frontend/account-manager-app/src/services/api.ts
@@ -13,6 +13,7 @@ import {
 OpenAPI.BASE = import.meta.env.VITE_FINERACT_API_URL;
 OpenAPI.USERNAME = import.meta.env.VITE_FINERACT_USERNAME;
 OpenAPI.PASSWORD = import.meta.env.VITE_FINERACT_PASSWORD;
+
 OpenAPI.HEADERS = {
 	"Fineract-Platform-TenantId": import.meta.env.VITE_FINERACT_TENANT_ID,
 };
