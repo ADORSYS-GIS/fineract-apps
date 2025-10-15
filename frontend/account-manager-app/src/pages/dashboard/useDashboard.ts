f44af28a73557abc6e78b3118802ac49d0a5b7c8
Elwizzy12 <chiahans123@gmail.com>
2025-10-15 09:44:45 +0100

    chore: added script to suppress warnings from generated code

--- frontend/account-manager-app/src/pages/dashboard/useDashboard.ts
+++ frontend/account-manager-app/src/pages/dashboard/useDashboard.ts
@@ -15,8 +15,8 @@ export const useDashboard = () => {
 		queryKey: ["clients", currentPage],
 		queryFn: () =>
 			fineractApi.clients.getV1Clients({
-				offset: (currentPage - 1) * 3,
-				limit: 3,
+				offset: (currentPage - 1) * 4,
+				limit: 4,
 				orderBy: "id",
 				sortOrder: "DESC",
 			}),
