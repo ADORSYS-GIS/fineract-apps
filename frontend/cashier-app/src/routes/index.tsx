0d365e946ace349260d545b2068fc8ae4160d198
NkwaTambe <nkwajudetambe@gmail.com>
2025-10-28 14:29:23 +0100

    chore: update translationHomepage

--- frontend/cashier-app/src/routes/index.tsx
+++ frontend/cashier-app/src/routes/index.tsx
@@ -3,7 +3,7 @@ import { useTranslation } from "react-i18next";
 
 function HomePage() {
 	const { t } = useTranslation();
-	return <div>{t("branchManagerHomePage")}</div>;
+	return <div>{t("cashierHomePage")}</div>;
 }
 
 export const Route = createFileRoute("/")({
