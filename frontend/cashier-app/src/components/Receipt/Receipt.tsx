c88b17863a35cc6a6d49b5bf1e7adc4b124b545a
nancymuyeh <nancymuyeh@gmail.com>
2025-11-20 17:04:43 +0100

    feat(loan): implement loan repaymant reciept

--- frontend/cashier-app/src/components/Receipt/Receipt.tsx
+++ frontend/cashier-app/src/components/Receipt/Receipt.tsx
@@ -1,27 +1,27 @@
 import { GetLoansLoanIdResponse } from "@fineract-apps/fineract-api";
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { ReceiptView } from "@/components/Receipt/Receipt.view";
 import { useReceipt } from "@/components/Receipt/useReceipt";
 
 interface ReceiptProps {
 	loan: GetLoansLoanIdResponse;
-	loanId: number;
 	transactionId: number;
 	onClose: () => void;
 }
 
 export const Receipt: React.FC<ReceiptProps> = ({
 	loan,
-	loanId,
 	transactionId,
 	onClose,
 }) => {
 	const { t } = useTranslation();
+	const [outputType, setOutputType] = useState<"PDF" | "XLS" | "HTML">("PDF");
 	const {
-		data: transaction,
+		data: receipt,
 		isLoading,
 		error,
-	} = useReceipt(loanId, transactionId);
+	} = useReceipt(transactionId, outputType);
 
 	if (isLoading) {
 		return <div>{t("loadingReceipt")}</div>;
@@ -31,11 +31,17 @@ export const Receipt: React.FC<ReceiptProps> = ({
 		return <div>{t("errorFetchingReceipt", { message: error.message })}</div>;
 	}
 
-	if (!transaction) {
-		return <div>{t("transactionNotFound")}</div>;
+	if (!receipt) {
+		return <div>{t("receiptNotFound")}</div>;
 	}
 
 	return (
-		<ReceiptView loan={loan} transaction={transaction} onClose={onClose} />
+		<ReceiptView
+			loan={loan}
+			receipt={receipt}
+			onClose={onClose}
+			outputType={outputType}
+			setOutputType={setOutputType}
+		/>
 	);
 };
