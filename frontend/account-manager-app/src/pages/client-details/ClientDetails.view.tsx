a63d87881a36f8c6444fd9065f52fe6dad4e01d1
Elwizzy12 <chiahans123@gmail.com>
2026-02-13 11:30:17 +0100

    fix:  distorted translation keys

--- frontend/account-manager-app/src/pages/client-details/ClientDetails.view.tsx
+++ frontend/account-manager-app/src/pages/client-details/ClientDetails.view.tsx
@@ -158,7 +158,8 @@ export const ClientDetailsView: FC<ReturnType<typeof useClientDetails>> = ({
 								</div>
 								<h2 className="text-2xl font-bold">{client?.displayName}</h2>
 								<p className="text-sm text-gray-500 mt-1">
-									{t("accountManagerClientDetails.clientId")} {client?.accountNo}
+									{t("accountManagerClientDetails.clientId")}{" "}
+									{client?.accountNo}
 								</p>
 								<p className="text-sm text-gray-500 mt-1">
 									{t("accountManagerClientDetails.phone")} {client?.mobileNo}
