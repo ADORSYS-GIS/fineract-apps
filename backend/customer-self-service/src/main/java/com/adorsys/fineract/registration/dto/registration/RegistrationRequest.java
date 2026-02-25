c1920976e121c5d73d7344cbb25f1210d5eed959
NkwaTambe <nkwajudetambe@gmail.com>
2026-02-25 09:42:27 +0100

    chore(): update project name and make email and optional field

--- backend/customer-self-service/src/main/java/com/adorsys/fineract/registration/dto/registration/RegistrationRequest.java
+++ backend/customer-self-service/src/main/java/com/adorsys/fineract/registration/dto/registration/RegistrationRequest.java
@@ -22,8 +22,6 @@ public class RegistrationRequest {
     @NotBlank(message = "Last name is required")
     private String lastName;
 
-    @NotBlank(message = "Email is required")
-    @Email(message = "Invalid email format")
     private String email;
 
     @NotBlank(message = "Phone number is required")
